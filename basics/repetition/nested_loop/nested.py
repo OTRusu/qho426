e01317392d720f3d40e2c5ac10fb9040f1db4754
Ovidiu Rusu <92996148+OTRusu@users.noreply.github.com>
2021-11-15 21:33:43 +0000

    Added code to demonstrate the use of one loop nested is another.

--- basics/repetition/nested_loop/nested.py
+++ basics/repetition/nested_loop/nested.py
@@ -1,9 +1,7 @@
-sequence = str(input('Please enter a sequence: '))
-marker = str(input('Please enter a marker: '))
-l1=[]
-for i, charachter in enumerate(sequence):
-  if charachter==marker:
-    l1.append(i)
-print(l1)
-print(l1[2]-l1[0]-1-1) 
-#example for string : abaaaabab wich will return how many a are between fir b aparicion and the third occurence of b excluding the b in the middle
+x = int(input('How many rows: '))
+y = int(input('How many coloumns: '))
+
+for i in range(x):
+  for j in range(y):
+    print(':)', end=' ')
+  print()
