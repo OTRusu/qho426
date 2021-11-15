5c0c19215429bdcfe65ce20547b4a113d1ed5256
Ovidiu Rusu <92996148+OTRusu@users.noreply.github.com>
2021-11-15 23:25:18 +0000

    Added code to demonstrate the use of built-in functions: len, ord.

---
+++ basics/functions/ascii_code.py
@@ -0,0 +1,9 @@
+print('Program Started!\n')
+x = input('Please enter a standard character: ')
+for i in x:
+  if len(x) < 2:
+    y = ord(x)
+    print('The ASCII code for %d is %d.' %(x,y))
+  else:
+    print('You have entered more than one charachter.')
+print('Program Ended!\n')
